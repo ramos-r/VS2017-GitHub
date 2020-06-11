d5b341df189cd9aace64bf4eb6d1153b2062e356
Bckymoon <bckymoon@hotmail.com>
2020-06-11 14:53:10 -0300

    Primeiro commit

---
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TesteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso do GitHub");
+            Console.ReadKey();
+        }
+    }
+}
